b863ab802bca103be7c96c434fec94f54fa0ac66
Dev-Steven <stevenchung650@gmail.com>
2020-08-30 13:55:06 -0700

    find, delete, update routes for posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let Post = require('../models/post.model');
 
+// get all posts
 router.route('/').get((req, res) => {
 	Post.find()
 		.then(posts => res.json(posts))
 		.catch(err => res.status(400).json('Error: ' + err));
 });
 
+// create a post
 router.route('/add').post((req, res) => {
 	const title = req.body.title;
 	const author = req.body.author;
@@ -27,6 +29,36 @@ router.route('/add').post((req, res) => {
 	newPost
 		.save()
 		.then(() => res.json('Post added!'))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+// find a post
+router.route('/:id').get((req, res) => {
+	Post.findById(req.params.id)
+		.then(post => res.json(post))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+// delete a post
+router.route('/:id').delete((req, res) => {
+	Post.findByIdAndDelete(req.params.id)
+		.then(() => res.json('Post deleted'))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+// edit a post
+router.route('/update/:id').post((req, res) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			post.title = req.body.title;
+			post.author = req.body.author;
+			post.body = req.body.body;
+			post.data = Date.parse(req.body.data);
+
+			post.save()
+				.then(() => res.json('Post updated!'))
+				.catch(err => res.status(400).json('Error: ' + err));
+		})
 		.catch(err => res.status(400).json('Error: ' + err));
 });
 
