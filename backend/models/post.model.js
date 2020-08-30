ae13c46bfda9f2a26d2fead162b477840aa71c0e
Dev-Steven <stevenchung650@gmail.com>
2020-08-29 17:45:01 -0700

    schemas setup

---
+++ backend/models/post.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		author: { type: String, required: true },
+		body: { type: String, required: true },
+		comments: [{ body: String, date: Date }],
+		date: { type: Date, default: Date.now },
+		meta: {
+			upvotes: Number,
+			downvotes: Number,
+			favs: Number,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
