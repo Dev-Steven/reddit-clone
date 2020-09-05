342ac806e74ce219db6ab25b364f61281dd3fba0
Dev-Steven <stevenchung650@gmail.com>
2020-09-04 23:39:03 -0700

    postCreate component styled and updated

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import './PostCreate.scss';
 
 const PostCreate = () => {
 	return (
-		<div>
-			<input type='text' placeholder='Title' />
-			<input type='text' placeholder='Text(optional)' />
-			<button>Cancel</button>
-			<button>Post</button>
+		<div className='form'>
+			<form action=''>
+				<input
+					className='form-control input-custom'
+					type='text'
+					placeholder='Title'
+				/>
+				<input
+					className='form-control input-custom'
+					type='text'
+					placeholder='Your Name'
+				/>
+				<input
+					className='form-control input-custom'
+					type='text'
+					placeholder='Body'
+				/>
+				<input className='form-control input-custom' type='date' />
+				<div className='buttons'>
+					<button className='btn btn-success'>Post</button>
+					<button className='btn btn-danger'>Cancel</button>
+				</div>
+			</form>
 		</div>
 	);
 };
