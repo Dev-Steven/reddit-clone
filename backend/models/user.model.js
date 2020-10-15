b029017935d67d0aec7635a3cfc0c8f73e772101
Dev-Steven <stevenchung650@gmail.com>
2020-10-14 17:09:13 -0700

    new userSchema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-	{
-		username: {
+const userSchema = new Schema({
+		name: {
 			type: String,
-			required: true,
-			unique: true,
-			trim: true,
-			minlength: 3,
+			required: true
 		},
-	},
-	{
-		timestamps: true,
-	}
-);
+		email: {
+			type: String,
+			required: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		date: {
+			type: Date,
+			default: Date.now
+		}
+});
 
 const User = mongoose.model('User', userSchema);
 
